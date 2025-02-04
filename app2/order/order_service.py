6f8feecaa36dcf56da20f8b8aa22fded0e37e6ef
nick-graves <nicholasg224@gmail.com>
2025-02-03 21:06:19 -0800

    second example

---
+++ app2/order/order_service.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/orders")
+def get_orders():
+    users = requests.get("http://user_service:5001/users").json()
+    return jsonify(orders=[{"user": user, "order": "Laptop"} for user in users["users"]])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002)
